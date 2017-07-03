3c4850c42225c3061bd8e4e0fda7829806478e74
Sergey Nemchinov <googoodemon@gmail.com>
2017-07-03 11:45:52 +0700

    Added toString() methods

--- src/main/java/ru/javawebinar/topjava/model/UserMeal.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMeal.java
@@ -40,4 +40,13 @@ public class UserMeal {
     public int getCalories() {
         return calories;
     }
+
+    @Override
+    public String toString() {
+        return "UserMeal{" +
+                "dateTime=" + dateTime +
+                ", description='" + description + '\'' +
+                ", calories=" + calories +
+                '}';
+    }
 }
